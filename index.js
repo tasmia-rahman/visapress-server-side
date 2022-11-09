c6b5025a2efe0f1f3adbf8d84b5af42a5e2d8ae8
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-11-09 11:46:14 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 var cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.aw5zzia.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send('API running');
