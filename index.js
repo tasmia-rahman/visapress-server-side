d11192fd2f503feaf1333b2a22d3e5bc470f8dc4
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-11-09 17:59:03 +0600

    services added

--- index.js
+++ index.js
@@ -6,10 +6,31 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middle wares
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.aw5zzia.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        const serviceCollection = client.db('visapressDB').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('API running');
